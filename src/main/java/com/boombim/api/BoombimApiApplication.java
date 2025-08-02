923493839ccabf4ab853155167b95bf10e110a38
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-02 22:44:17 +0900

    initial commit

---
+++ src/main/java/com/boombim/api/BoombimApiApplication.java
@@ -0,0 +1,13 @@
+package com.boombim.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoombimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoombimApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boombim/api/BoombimApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boombim.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoombimApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
