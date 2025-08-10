8af9b097bc671a343f13e3e4947f4d457de8658a
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-10 20:24:34 +0900

    [Feat] Add response message for successful retrieval of all official places in `ResponseMessage.java`

--- src/main/java/boombimapi/global/response/ResponseMessage.java
+++ src/main/java/boombimapi/global/response/ResponseMessage.java
@@ -1,4 +1,14 @@
 package boombimapi.global.response;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ResponseMessage {
+
+    GET_ALL_OFFICIAL_PLACES_CENTER_SUCCESS("모든 공식 장소 중앙 좌표 조회 성공");
+
+    private final String message;
+
 }
