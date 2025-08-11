891763c7b005b33c5b945cdb04fbd2b960e3e022
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-11 09:56:15 +0900

    [Feat] Implement generic API response wrapper with factory method in `ApiResponse.java`

--- src/main/java/boombimapi/global/response/ApiResponse.java
+++ src/main/java/boombimapi/global/response/ApiResponse.java
@@ -1,5 +1,25 @@
 package boombimapi.global.response;
 
-public record ApiResponse() {
+import org.springframework.http.HttpStatus;
+
+public record ApiResponse<T>(
+    int code,
+    String status,
+    String message,
+    T data
+) {
+
+    public static <T> ApiResponse<T> of(
+        HttpStatus status,
+        ResponseMessage message,
+        T data
+    ) {
+        return new ApiResponse<>(
+            status.value(),
+            status.getReasonPhrase(),
+            message.getMessage(),
+            data
+        );
+    }
 
 }
