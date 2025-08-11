51fefe0ae099e41f7337d395e738d562ffa7e992
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-11 09:06:57 +0900

    [Feat] Create JPA entity for congestion_levels table in `CongestionLevel.java`

--- src/main/java/boombimapi/domain/congestion/entity/CongestionLevel.java
+++ src/main/java/boombimapi/domain/congestion/entity/CongestionLevel.java
@@ -1,5 +1,49 @@
 package boombimapi.domain.congestion.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "congestion_levels")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CongestionLevel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String message;
+
+    @Builder
+    private CongestionLevel(
+        String name,
+        String message
+    ) {
+        this.name = name;
+        this.message = message;
+    }
+
+    private static CongestionLevel of(
+        String name,
+        String message
+    ) {
+        return CongestionLevel.builder()
+            .name(name)
+            .message(message)
+            .build();
+    }
 
 }
