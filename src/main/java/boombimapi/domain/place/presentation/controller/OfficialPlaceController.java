3d4406cd19186d217055980a0fe47f3f3dae1eeb
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-11 16:51:14 +0900

    [Feat] Add endpoint to return markers inside viewport in `OfficialPlaceController.java`

--- src/main/java/boombimapi/domain/place/presentation/controller/OfficialPlaceController.java
+++ src/main/java/boombimapi/domain/place/presentation/controller/OfficialPlaceController.java
@@ -1,5 +1,38 @@
 package boombimapi.domain.place.presentation.controller;
 
+import boombimapi.domain.place.application.service.impl.OfficialPlaceServiceImpl;
+import boombimapi.domain.place.presentation.dto.request.Viewport;
+import boombimapi.domain.place.presentation.dto.response.MapMarker;
+import boombimapi.global.response.ApiResponse;
+import boombimapi.global.response.ResponseMessage;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/official-place")
 public class OfficialPlaceController {
+
+    private final OfficialPlaceServiceImpl officialPlaceService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<List<MapMarker>>> getMarkersInViewport(
+        @RequestBody Viewport viewport
+    ) {
+        return ResponseEntity.ok(
+            ApiResponse.of(
+                HttpStatus.OK,
+                ResponseMessage.GET_ALL_OFFICIAL_PLACES_CENTER_SUCCESS,
+                officialPlaceService.getMarkersInViewport(viewport)
+            )
+        );
+    }
+
 
 }
