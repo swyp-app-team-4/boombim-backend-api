30e45d566ccf2907a6e19d86a17a2677a230d8b9
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-11 19:52:25 +0900

    [Refactor] Update to use PlaceService, Swagger annotations and BaseResponse in `OfficialPlaceController.java`

--- src/main/java/boombimapi/domain/place/presentation/controller/OfficialPlaceController.java
+++ src/main/java/boombimapi/domain/place/presentation/controller/OfficialPlaceController.java
@@ -2,10 +2,14 @@ package boombimapi.domain.place.presentation.controller;
 
 import static boombimapi.global.response.ResponseMessage.*;
 
-import boombimapi.domain.place.application.service.impl.OfficialPlaceServiceImpl;
+import boombimapi.domain.place.application.service.PlaceService;
 import boombimapi.domain.place.presentation.dto.request.Viewport;
 import boombimapi.domain.place.presentation.dto.response.MapMarker;
-import boombimapi.global.response.ApiResponse;
+import boombimapi.global.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,16 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/official-place")
+@Tag(name = "OfficialPlace", description = "공식 장소 API")
 public class OfficialPlaceController {
 
-    private final OfficialPlaceServiceImpl officialPlaceService;
+    private final PlaceService officialPlaceService;
 
+    @Operation(summary = "뷰포트 내 공식 장소 마커 조회 API", description = "뷰포트 내에 위치한 공식 장소들의 좌표를 리스트로 반환합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "공식 장소 마커 조회 성공")
+    })
     @PostMapping
-    public ResponseEntity<ApiResponse<List<MapMarker>>> getMarkersInViewport(
+    public ResponseEntity<BaseResponse<List<MapMarker>>> getMarkersInViewport(
         @RequestBody Viewport viewport
     ) {
         return ResponseEntity.ok(
-            ApiResponse.of(
+            BaseResponse.of(
                 HttpStatus.OK,
                 GET_OFFICIAL_PLACES_WITHIN_VIEWPORT_SUCCESS,
                 officialPlaceService.getMarkersInViewport(viewport)
