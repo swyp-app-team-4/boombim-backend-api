4c73b8005853bf20648f48b8e53960bb840e4ea7
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-11 13:41:37 +0900

    [Feat] Create DTO for map marker details in `MapMarker.java`

--- src/main/java/boombimapi/domain/place/presentation/dto/MapMarker.java
+++ src/main/java/boombimapi/domain/place/presentation/dto/MapMarker.java
@@ -1,5 +1,10 @@
 package boombimapi.domain.place.presentation.dto;
 
-public record MapMarker() {
+public record MapMarker(
+    Long id,
+    String name,
+    Double latitude,
+    Double longitude
+) {
 
 }
