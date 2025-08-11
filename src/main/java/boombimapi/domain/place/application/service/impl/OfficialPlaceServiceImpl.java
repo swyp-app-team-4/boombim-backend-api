b70fbdace2ddb41a66246aea01650ac05d86f8e6
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-11 17:19:09 +0900

    [Feat] Implement viewport based marker retrieval in `OfficialPlaceServiceImpl.java`

--- src/main/java/boombimapi/domain/place/application/service/impl/OfficialPlaceServiceImpl.java
+++ src/main/java/boombimapi/domain/place/application/service/impl/OfficialPlaceServiceImpl.java
@@ -1,5 +1,47 @@
 package boombimapi.domain.place.application.service.impl;
 
-public class OfficialPlaceServiceImpl {
+import boombimapi.domain.place.application.service.PlaceService;
+import boombimapi.domain.place.domain.entity.OfficialPlace;
+import boombimapi.domain.place.domain.repository.OfficialPlaceRepository;
+import boombimapi.domain.place.presentation.dto.request.Viewport;
+import boombimapi.domain.place.presentation.dto.response.MapMarker;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OfficialPlaceServiceImpl implements PlaceService {
+
+    private final OfficialPlaceRepository officialPlaceRepository;
+
+    @Override
+    public List<MapMarker> getMarkersInViewport(
+        Viewport viewport
+    ) {
+
+        double latitude1 = viewport.topLeft().latitude();
+        double longitude1 = viewport.topLeft().longitude();
+        double latitude2 = viewport.bottomRight().latitude();
+        double longitude2 = viewport.bottomRight().longitude();
+
+        double minLatitude = Math.min(latitude1, latitude2);
+        double maxLatitude = Math.max(latitude1, latitude2);
+        double minLongitude = Math.min(longitude1, longitude2);
+        double maxLongitude = Math.max(longitude1, longitude2);
+
+        List<OfficialPlace> officialPlaces = officialPlaceRepository.findInViewport(
+            minLatitude,
+            maxLatitude,
+            minLongitude,
+            maxLongitude
+        );
+
+        return officialPlaces.stream()
+            .map(MapMarker::fromEntity)
+            .toList();
+
+    }
 }
