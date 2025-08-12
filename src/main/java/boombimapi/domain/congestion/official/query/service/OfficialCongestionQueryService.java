37302dc0901ba76faf22ef2350d3031538d59d87
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-12 14:44:09 +0900

    [Feat] Add service to fetch latest official congestion with exception handling in `OfficialCongestionQueryService.java`

--- src/main/java/boombimapi/domain/congestion/official/query/service/OfficialCongestionQueryService.java
+++ src/main/java/boombimapi/domain/congestion/official/query/service/OfficialCongestionQueryService.java
@@ -1,5 +1,27 @@
 package boombimapi.domain.congestion.official.query.service;
 
+import static boombimapi.global.infra.exception.error.ErrorCode.*;
+
+import boombimapi.domain.congestion.official.query.dto.OfficialCongestionResponse;
+import boombimapi.domain.congestion.official.query.repository.OfficialCongestionQueryRepository;
+import boombimapi.global.infra.exception.error.BoombimException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OfficialCongestionQueryService {
+
+    private final OfficialCongestionQueryRepository officialCongestionQueryRepository;
+
+    public OfficialCongestionResponse getLatestOfficialCongestion(
+        Long officialPlaceId
+    ) {
+        return officialCongestionQueryRepository
+            .findLatestOfficialCongestionByOfficialPlaceId(officialPlaceId)
+            .orElseThrow(() -> new BoombimException(OFFICIAL_CONGESTION_NOT_FOUND));
+    }
 
 }
