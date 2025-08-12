ff255a51290c9ea09e15606c6c074bef5c7a4059
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-12 14:40:07 +0900

    [Feat] Implement QueryDSL based repository for fetching latest official congestion in `OfficialCongestionQueryRepository.java`

--- src/main/java/boombimapi/domain/congestion/official/query/repository/OfficialCongestionQueryRepository.java
+++ src/main/java/boombimapi/domain/congestion/official/query/repository/OfficialCongestionQueryRepository.java
@@ -1,5 +1,51 @@
 package boombimapi.domain.congestion.official.query.repository;
 
+import static boombimapi.domain.congestion.official.entity.QOfficialCongestion.officialCongestion;
+import static boombimapi.domain.congestion.common.entity.QCongestionLevel.congestionLevel;
+import static boombimapi.domain.place.domain.entity.QOfficialPlace.officialPlace;
+
+import boombimapi.domain.congestion.official.query.dto.OfficialCongestionResponse;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * QueryDSL JPA를 이용한 조회(Query) 전용 리포지토리
+ */
+@Repository
+@RequiredArgsConstructor
 public class OfficialCongestionQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public Optional<OfficialCongestionResponse> findLatestOfficialCongestionByOfficialPlaceId(
+        Long officialPlaceId
+    ) {
+        OfficialCongestionResponse officialCongestionResponse = queryFactory.select(
+                Projections.constructor(
+                    OfficialCongestionResponse.class,
+                    officialCongestion.id,
+                    officialPlace.id,
+                    officialPlace.name,
+                    congestionLevel.id,
+                    congestionLevel.name,
+                    congestionLevel.message,
+                    officialCongestion.populationMin,
+                    officialCongestion.populationMax,
+                    officialCongestion.observedAt
+                )
+            )
+            .from(officialCongestion)
+            .join(officialCongestion.officialPlace, officialPlace)
+            .join(officialCongestion.congestionLevel, congestionLevel)
+            .where(officialPlace.id.eq(officialPlaceId))
+            .orderBy(officialCongestion.observedAt.desc())
+            .limit(1)
+            .fetchOne();
+
+        return Optional.ofNullable(officialCongestionResponse);
+    }
 
 }
