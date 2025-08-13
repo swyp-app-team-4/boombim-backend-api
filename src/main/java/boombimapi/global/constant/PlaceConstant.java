3d3db94d303c00616bc2bc8375408547a2f4333c
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-13 16:50:41 +0900

    [Feat] Add constant and prevent instantiation in `PlaceConstant.java`

--- src/main/java/boombimapi/global/constant/PlaceConstant.java
+++ src/main/java/boombimapi/global/constant/PlaceConstant.java
@@ -2,4 +2,9 @@ package boombimapi.global.constant;
 
 public final class PlaceConstant {
 
+    private PlaceConstant() {
+        throw new UnsupportedOperationException("상수 클래스는 인스턴스화 할 수 없습니다.");
+    }
+
+    public static final double EARTH_RADIUS_METERS = 6_371_000;
 }
