5f8a8080e14f7c65ebd949d3384d0e66db75b8d3
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-15 16:02:46 +0900

    [Feat] Add derived query method for viewport coordinate filtering in `OfficialPlaceRepository.java`

--- src/main/java/boombimapi/domain/place/repository/OfficialPlaceRepository.java
+++ src/main/java/boombimapi/domain/place/repository/OfficialPlaceRepository.java
@@ -1,8 +1,17 @@
 package boombimapi.domain.place.repository;
 
 import boombimapi.domain.place.entity.OfficialPlace;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OfficialPlaceRepository extends JpaRepository<OfficialPlace, Long> {
+
+    // TODO: JPQL/Native 없이 파생 메서드로 우선 구현
+    List<OfficialPlace> findByCentroidLatitudeBetweenAndCentroidLongitudeBetween(
+        double minLatitude,
+        double maxLatitude,
+        double minLongitude,
+        double maxLongitude
+    );
 
 }
