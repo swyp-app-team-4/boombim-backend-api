41a2b94750a88eea26333f8e9543851f2d81b77b
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-15 13:31:04 +0900

    [Feat] Add `OfficialPlaceRepository.java`

---
+++ src/main/java/boombimapi/domain/place/repository/OfficialPlaceRepository.java
@@ -0,0 +1,8 @@
+package boombimapi.domain.place.repository;
+
+import boombimapi.domain.place.entity.OfficialPlace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfficialPlaceRepository extends JpaRepository<OfficialPlace, Long> {
+
+}
