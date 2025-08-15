70ee01a918a45ee75361312873a407f9d2e931cc
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-16 00:13:32 +0900

    [Feat] Enable JPA auditing in `BoombimApiApplication.java`
    # Conflicts:
    #	src/main/java/boombimapi/BoombimApiApplication.java

--- src/main/java/boombimapi/BoombimApiApplication.java
+++ src/main/java/boombimapi/BoombimApiApplication.java
@@ -4,13 +4,15 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
-@SpringBootApplication
-@EnableFeignClients
 @EnableScheduling
+@EnableJpaAuditing
+@EnableFeignClients
+@SpringBootApplication
 public class BoombimApiApplication {
 
     public static void main(String[] args) {
