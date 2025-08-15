c754693eff65259123bad59cb5c7b5705244266c
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-15 16:06:01 +0900

    [Feat] Implement record with fields in `ViewportResponse.java`

--- src/main/java/boombimapi/domain/place/dto/response/ViewportResponse.java
+++ src/main/java/boombimapi/domain/place/dto/response/ViewportResponse.java
@@ -1,5 +1,14 @@
 package boombimapi.domain.place.dto.response;
 
-public record ViewportResponse() {
+import boombimapi.global.dto.Coordinate;
+
+public record ViewportResponse(
+    Long id,
+    String name,
+    Coordinate coordinate,
+    double distance,
+    String congestionLevelName,
+    String congestionMessage
+) {
 
 }
