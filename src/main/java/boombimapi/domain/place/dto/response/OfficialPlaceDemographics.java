6b8b2aec59eb4e56b3fb19f13bd549f80009f665
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-15 17:40:38 +0900

    [Feat] Implement OfficialPlaceDemographics DTO with mapping from entity in `OfficialPlaceDemographics.java`

--- src/main/java/boombimapi/domain/place/dto/response/OfficialPlaceDemographics.java
+++ src/main/java/boombimapi/domain/place/dto/response/OfficialPlaceDemographics.java
@@ -1,5 +1,23 @@
 package boombimapi.domain.place.dto.response;
 
-public record OfficialPlaceDemographics() {
+import boombimapi.domain.congestion.entity.OfficialCongestionDemographics;
+import boombimapi.domain.congestion.entity.OfficialCongestionForecast;
+
+// TODO: 여기 열거형으로 처리하기
+public record OfficialPlaceDemographics(
+    String category,
+    String subCategory,
+    Double rate
+) {
+
+    public static OfficialPlaceDemographics from(
+        OfficialCongestionDemographics entity
+    ) {
+        return new OfficialPlaceDemographics(
+            entity.getCategory(),
+            entity.getSubCategory(),
+            entity.getRate()
+        );
+    }
 
 }
