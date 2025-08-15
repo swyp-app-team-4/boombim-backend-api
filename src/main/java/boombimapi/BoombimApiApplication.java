1f8deaeef4f24b83623ad587a90c7b4b8973356a
chltmdgh522 <chltmdgh517@naver.com>
2025-08-15 16:04:41 +0900

    [Fix] Add @EnableScheduling annotation to scheduler configuration

--- src/main/java/boombimapi/BoombimApiApplication.java
+++ src/main/java/boombimapi/BoombimApiApplication.java
@@ -3,9 +3,11 @@ package boombimapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class BoombimApiApplication {
 
     public static void main(String[] args) {
