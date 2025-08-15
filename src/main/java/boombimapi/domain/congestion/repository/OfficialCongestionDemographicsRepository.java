845dad8acd8f3669db990ca73b690e378a075e9c
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-15 16:40:49 +0900

    [Feat] Add `OfficialCongestionDemographicsRepository.java`

---
+++ src/main/java/boombimapi/domain/congestion/repository/OfficialCongestionDemographicsRepository.java
@@ -0,0 +1,8 @@
+package boombimapi.domain.congestion.repository;
+
+import boombimapi.domain.congestion.entity.OfficialCongestionDemographics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfficialCongestionDemographicsRepository extends JpaRepository<OfficialCongestionDemographics, Long> {
+
+}
