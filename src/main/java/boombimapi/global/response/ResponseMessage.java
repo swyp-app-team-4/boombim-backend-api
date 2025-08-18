16f3938c1958b5663cc78d72274d478564961b18
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-18 23:08:47 +0900

    [Feat] Add RESOLVE_MEMBER_PLACE_SUCCESS message in `ResponseMessage.java`

--- src/main/java/boombimapi/global/response/ResponseMessage.java
+++ src/main/java/boombimapi/global/response/ResponseMessage.java
@@ -10,7 +10,10 @@ public enum ResponseMessage {
     // official
     GET_OFFICIAL_PLACES_IN_VIEWPORT_SUCCESS("뷰포트 내 공식 장소 조회 성공"),
     GET_LATEST_OFFICIAL_CONGESTION_SUCCESS("최신 공식 혼잡도 조회 성공"),
-    GET_OFFICIAL_PLACE_OVERVIEW_SUCCESS("공식 장소 개요 조회 성공");
+    GET_OFFICIAL_PLACE_OVERVIEW_SUCCESS("공식 장소 개요 조회 성공"),
+
+    // member place
+    RESOLVE_MEMBER_PLACE_SUCCESS("사용자 장소 ID 조회 성공");
 
     private final String message;
 
