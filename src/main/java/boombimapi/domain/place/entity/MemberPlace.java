a3c90f955cc9f0b2ed2e5739434a5cbc0895a9a9
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-18 22:57:12 +0900

    [Feat] Implement MemberPlace entity with fields and builder in `Memberplace.java`

--- src/main/java/boombimapi/domain/place/entity/MemberPlace.java
+++ src/main/java/boombimapi/domain/place/entity/MemberPlace.java
@@ -1,5 +1,57 @@
 package boombimapi.domain.place.entity;
 
-public class MemberPlace {
+import boombimapi.global.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "member_places")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberPlace extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "uuid", length = 32, nullable = false, unique = true)
+    private String uuid;
+
+    @Column(name = "latitude", nullable = false)
+    private Double latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private Double longitude;
+
+    @Builder
+    private MemberPlace(
+        String uuid,
+        Double latitude,
+        Double longitude
+    ) {
+        this.uuid = uuid;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public static MemberPlace of(
+        String uuid,
+        Double latitude,
+        Double longitude
+    ) {
+        return MemberPlace.builder()
+            .uuid(uuid)
+            .latitude(latitude)
+            .longitude(longitude)
+            .build();
+    }
 
 }
