82cac275f616844401e440a4d3bba234b03c4d01
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-18 23:08:21 +0900

    [Feat] Add static factory method from() in `ResolveMemberPlaceResponse.java`

--- src/main/java/boombimapi/domain/place/dto/response/ResolveMemberPlaceResponse.java
+++ src/main/java/boombimapi/domain/place/dto/response/ResolveMemberPlaceResponse.java
@@ -1,7 +1,17 @@
 package boombimapi.domain.place.dto.response;
 
+import boombimapi.domain.place.entity.MemberPlace;
+
 public record ResolveMemberPlaceResponse(
     Long id
 ) {
+
+    public static ResolveMemberPlaceResponse from(
+        MemberPlace memberPlace
+    ) {
+        return new ResolveMemberPlaceResponse(
+            memberPlace.getId()
+        );
+    }
 
 }
