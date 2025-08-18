cc4fadc15ee43ebd138e6ad0f505fd9d4936a3d9
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-18 14:28:53 +0900

    [Feat] Add `MemberPlaceRepository.java`

---
+++ src/main/java/boombimapi/domain/place/repository/MemberPlaceRepository.java
@@ -0,0 +1,8 @@
+package boombimapi.domain.place.repository;
+
+import boombimapi.domain.place.entity.MemberPlace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberPlaceRepository extends JpaRepository<MemberPlace, Long> {
+
+}
