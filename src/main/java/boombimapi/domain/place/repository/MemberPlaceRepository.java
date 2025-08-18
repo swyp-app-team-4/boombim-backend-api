b421eca1e38286d814bf831cfab951ee3a27ff11
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-18 23:05:49 +0900

    [Feat] Add findByUuid() method in `MemberPlaceRepository.java`

--- src/main/java/boombimapi/domain/place/repository/MemberPlaceRepository.java
+++ src/main/java/boombimapi/domain/place/repository/MemberPlaceRepository.java
@@ -1,8 +1,10 @@
 package boombimapi.domain.place.repository;
 
 import boombimapi.domain.place.entity.MemberPlace;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberPlaceRepository extends JpaRepository<MemberPlace, Long> {
 
+    Optional<MemberPlace> findByUuid(String uuid);
 }
