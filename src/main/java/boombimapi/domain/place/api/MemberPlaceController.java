3d7680410727c5a8b091d40566a73f521461b4c9
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-18 22:58:23 +0900

    [Feat] Implement MemberPlaceController with resolve endpoint in `MemberPlaceController.java`/

--- src/main/java/boombimapi/domain/place/api/MemberPlaceController.java
+++ src/main/java/boombimapi/domain/place/api/MemberPlaceController.java
@@ -1,5 +1,37 @@
 package boombimapi.domain.place.api;
 
+import static boombimapi.global.response.ResponseMessage.*;
+
+import boombimapi.domain.place.application.MemberPlaceService;
+import boombimapi.domain.place.dto.request.ResolveMemberPlaceRequest;
+import boombimapi.domain.place.dto.response.ResolveMemberPlaceResponse;
+import boombimapi.global.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member-place")
 public class MemberPlaceController {
+
+    private final MemberPlaceService memberPlaceService;
+
+    @PostMapping("/resolve")
+    public ResponseEntity<BaseResponse<ResolveMemberPlaceResponse>> resolveMemberPlace(
+        @RequestBody ResolveMemberPlaceRequest request
+    ) {
+        return ResponseEntity.ok(
+            BaseResponse.of(
+                HttpStatus.OK,
+                RESOLVE_MEMBER_PLACE_SUCCESS,
+                memberPlaceService.resolveMemberPlace(request)
+            )
+        );
+    }
 
 }
