e70414afe4bc6bbccefb0de03c84eba993ce6af4
chltmdgh522 <chltmdgh517@naver.com>
2025-08-19 16:16:20 +0900

    [Fix] Update DTO for My Page Section 1 API

--- src/main/java/boombimapi/domain/member/presentation/dto/member/res/GetMemberRes.java
+++ src/main/java/boombimapi/domain/member/presentation/dto/member/res/GetMemberRes.java
@@ -23,16 +23,16 @@ public record GetMemberRes(
         Long voteCnt,
 
         @Schema(description = "작성한 답변 수", example = "34")
-        Long answerCnt
+        Long questionCnt
 ) {
-    public static GetMemberRes of(Member user, Long voteCnt, Long answerCnt) {
+    public static GetMemberRes of(Member user, Long voteCnt, Long questionCnt) {
         return new GetMemberRes(
                 user.getName(),
                 user.getProfile(),
                 user.getEmail(),
                 user.getSocialProvider(),
                 voteCnt,
-                answerCnt
+                questionCnt
         );
     }
 }
