9e9b9fdb65929dd557008976b74057f20babf9f2
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-19 16:26:27 +0900

    [Feat] Add `CongestionLevelRepository.java`

---
+++ src/main/java/boombimapi/domain/congestion/repository/CongestionLevelRepository.java
@@ -0,0 +1,8 @@
+package boombimapi.domain.congestion.repository;
+
+import boombimapi.domain.congestion.entity.CongestionLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CongestionLevelRepository extends JpaRepository<CongestionLevel, Long> {
+
+}
