91e9f56dc52e76437362d9ebc6c19fd42a2eed96
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-21 08:44:16 +0900

    [Feat] Implement record with factory method in `ViewportClusterNodeResponse.java`

--- src/main/java/boombimapi/domain/place/dto/response/ViewportClusterNodeResponse.java
+++ src/main/java/boombimapi/domain/place/dto/response/ViewportClusterNodeResponse.java
@@ -1,4 +1,23 @@
 package boombimapi.domain.place.dto.response;
 
-public record ViewportClusterNodeResponse() {
+import boombimapi.domain.place.dto.type.MarkerType;
+import boombimapi.global.dto.Coordinate;
+
+public record ViewportClusterNodeResponse(
+    MarkerType type,
+    Coordinate coordinate,
+    int clusterSize
+) {
+
+    public static ViewportClusterNodeResponse of(
+        Coordinate coordinate,
+        int clusterSize
+    ) {
+        return new ViewportClusterNodeResponse(
+            MarkerType.CLUSTER,
+            coordinate,
+            clusterSize
+        );
+    }
+
 }
