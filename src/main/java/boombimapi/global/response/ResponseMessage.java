7ac45add9b5f88ba5a4c700dfe121f6b4b98cf18
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-21 08:44:37 +0900

    [Feat] Add member place and congestion response messages in `ResponseMessage.java`

--- src/main/java/boombimapi/global/response/ResponseMessage.java
+++ src/main/java/boombimapi/global/response/ResponseMessage.java
@@ -13,7 +13,11 @@ public enum ResponseMessage {
     GET_OFFICIAL_PLACE_OVERVIEW_SUCCESS("공식 장소 개요 조회 성공"),
 
     // member place
-    RESOLVE_MEMBER_PLACE_SUCCESS("사용자 장소 ID 조회 성공");
+    GET_MEMBER_PLACES_IN_VIEWPORT_SUCCESS("뷰포트 내 사용자 장소 조회 성공"),
+    RESOLVE_MEMBER_PLACE_SUCCESS("사용자 장소 ID 조회 성공"),
+
+    // member congestion
+    CREATE_MEMBER_CONGESTION_SUCCESS("사용자 혼잡도 생성 성공");
 
     private final String message;
 
