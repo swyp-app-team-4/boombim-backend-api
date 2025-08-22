3d925c483a2ae252c0317d1c836e52f2b2bf199f
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-22 13:24:40 +0900

    [Refactor] Change type from Long to Integer in `CongestionLevelRepository.java`

--- src/main/java/boombimapi/domain/congestion/repository/CongestionLevelRepository.java
+++ src/main/java/boombimapi/domain/congestion/repository/CongestionLevelRepository.java
@@ -3,6 +3,6 @@ package boombimapi.domain.congestion.repository;
 import boombimapi.domain.congestion.entity.CongestionLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CongestionLevelRepository extends JpaRepository<CongestionLevel, Long> {
+public interface CongestionLevelRepository extends JpaRepository<CongestionLevel, Integer> {
 
 }
