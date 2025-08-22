089c219b1003d01c419a71e5ba28222fe0649c81
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-22 13:21:53 +0900

    [Feat] Implement with factory method in `CreateMemberCongestionResponse.java`

--- src/main/java/boombimapi/domain/congestion/dto/response/CreateMemberCongestionResponse.java
+++ src/main/java/boombimapi/domain/congestion/dto/response/CreateMemberCongestionResponse.java
@@ -1,5 +1,19 @@
 package boombimapi.domain.congestion.dto.response;
 
-public record CreateMemberCongestionResponse() {
+import boombimapi.domain.congestion.entity.MemberCongestion;
+
+public record CreateMemberCongestionResponse(
+    Long memberCongestionId,
+    String memberPlaceName
+) {
+
+    public static CreateMemberCongestionResponse from(
+        MemberCongestion memberCongestion
+    ) {
+        return new CreateMemberCongestionResponse(
+            memberCongestion.getId(),
+            memberCongestion.getMemberPlace().getName()
+        );
+    }
 
 }
