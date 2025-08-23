e8aae1b450b4db2d5d790f966adc272cd5462a27
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-23 19:28:57 +0900

    [Feat] Add @ConfigurationPropertiesScan annotation in `BoombimApiApplication.java`

--- src/main/java/boombimapi/BoombimApiApplication.java
+++ src/main/java/boombimapi/BoombimApiApplication.java
@@ -3,6 +3,7 @@ package boombimapi;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +13,7 @@ import java.util.TimeZone;
 @EnableScheduling
 @EnableJpaAuditing
 @EnableFeignClients
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class BoombimApiApplication {
 
