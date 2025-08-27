82de73b95bab4cb84627b41e31e46c84aa33556f
chltmdgh522 <chltmdgh517@naver.com>
2025-08-27 17:53:16 +0900

    [Fix] Link vote entity with place entity

--- src/main/java/boombimapi/domain/congestion/dto/request/CreateMemberCongestionRequest.java
+++ src/main/java/boombimapi/domain/congestion/dto/request/CreateMemberCongestionRequest.java
@@ -1,11 +1,26 @@
 package boombimapi.domain.congestion.dto.request;
 
 public record CreateMemberCongestionRequest(
-    Long memberPlaceId,
-    Integer congestionLevelId,
-    String congestionMessage,
-    Double latitude,
-    Double longitude
+        Long memberPlaceId,
+        Integer congestionLevelId,
+        String congestionMessage,
+        Double latitude,
+        Double longitude
 ) {
 
+    public static CreateMemberCongestionRequest of(
+            Long memberPlaceId,
+            Integer congestionLevelId,
+            String congestionMessage,
+            Double latitude,
+            Double longitude
+    ) {
+        return new CreateMemberCongestionRequest(
+                memberPlaceId,
+                congestionLevelId,
+                congestionMessage,
+                latitude,
+                longitude
+        );
+    }
 }
--- src/main/java/boombimapi/domain/place/entity/MemberPlace.java
+++ src/main/java/boombimapi/domain/place/entity/MemberPlace.java
@@ -1,16 +1,16 @@
 package boombimapi.domain.place.entity;
 
+import boombimapi.domain.vote.domain.entity.Vote;
+import boombimapi.domain.vote.domain.entity.VoteDuplication;
 import boombimapi.global.base.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,6 +21,9 @@ public class MemberPlace extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany(mappedBy = "memberPlace", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Vote> votes  = new ArrayList<>();
 
     @Column(name = "uuid", length = 32, nullable = false, unique = true)
     private String uuid;
