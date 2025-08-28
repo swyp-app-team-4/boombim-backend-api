d0957988053f94d9e29eaa78ff8ca22ac42f3f52
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-29 00:25:10 +0900

    [Feat] Implement Favorite entity with relations to Member and MemberPlace in `Favorite.java`

--- src/main/java/boombimapi/domain/favorite/entity/Favorite.java
+++ src/main/java/boombimapi/domain/favorite/entity/Favorite.java
@@ -1,5 +1,56 @@
 package boombimapi.domain.favorite.entity;
 
-public class Favorite {
+import boombimapi.domain.member.domain.entity.Member;
+import boombimapi.domain.place.entity.MemberPlace;
+import boombimapi.global.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "favorites")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Favorite extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_place_id", nullable = false)
+    private MemberPlace memberPlace;
+
+    @Builder
+    private Favorite(
+        Member member,
+        MemberPlace memberPlace
+    ) {
+        this.member = member;
+        this.memberPlace = memberPlace;
+    }
+
+    public static Favorite of(
+        Member member,
+        MemberPlace memberPlace
+    ) {
+        return Favorite.builder()
+            .member(member)
+            .memberPlace(memberPlace)
+            .build();
+    }
 
 }
