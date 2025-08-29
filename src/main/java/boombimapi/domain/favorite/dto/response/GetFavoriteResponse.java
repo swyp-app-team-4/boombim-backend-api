17de3a8fbf26aef49299e366cdb8e515f4f351fe
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-29 18:42:36 +0900

    [Feat] Implement record with factory method in `GetFavoriteResponse.java`

--- src/main/java/boombimapi/domain/favorite/dto/response/GetFavoriteResponse.java
+++ src/main/java/boombimapi/domain/favorite/dto/response/GetFavoriteResponse.java
@@ -1,5 +1,18 @@
 package boombimapi.domain.favorite.dto.response;
 
-public record GetFavoriteResponse() {
+public record GetFavoriteResponse(
+    Long favoriteId,
+    Long memberPlaceId
+) {
+
+    public static GetFavoriteResponse from(
+        Long favoriteId,
+        Long memberPlaceId
+    ) {
+        return new GetFavoriteResponse(
+            favoriteId,
+            memberPlaceId
+        );
+    }
 
 }
