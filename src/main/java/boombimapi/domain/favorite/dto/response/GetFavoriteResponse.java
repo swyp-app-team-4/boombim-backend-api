a39ceaa93cced3be37bf22edb7a1b661f28b04ae
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-29 20:49:48 +0900

    [Feat] Expand fields in `GetFavoriteResponse.java`

--- src/main/java/boombimapi/domain/favorite/dto/response/GetFavoriteResponse.java
+++ src/main/java/boombimapi/domain/favorite/dto/response/GetFavoriteResponse.java
@@ -1,17 +1,37 @@
 package boombimapi.domain.favorite.dto.response;
 
+import java.time.LocalDateTime;
+
 public record GetFavoriteResponse(
     Long favoriteId,
-    Long memberPlaceId
+    Long memberPlaceId,
+    Double latitude,
+    Double longitude,
+    String memberPlaceName,
+    String congestionLevelName,
+    String congestionMessage,
+    LocalDateTime observedAt
 ) {
 
-    public static GetFavoriteResponse from(
+    public static GetFavoriteResponse of(
         Long favoriteId,
-        Long memberPlaceId
+        Long memberPlaceId,
+        Double latitude,
+        Double longitude,
+        String memberPlaceName,
+        String congestionLevelName,
+        String congestionMessage,
+        LocalDateTime observedAt
     ) {
         return new GetFavoriteResponse(
             favoriteId,
-            memberPlaceId
+            memberPlaceId,
+            latitude,
+            longitude,
+            memberPlaceName,
+            congestionLevelName,
+            congestionMessage,
+            observedAt
         );
     }
 
