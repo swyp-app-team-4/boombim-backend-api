88b6c1ad65768a73fdf5fbf448ad2a75bd4373b7
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-03 01:28:23 +0900

    [Feat] Implement response record for member congestion item in `MemberCongestionItemResponse.java`

--- src/main/java/boombimapi/domain/congestion/dto/response/MemberCongestionItemResponse.java
+++ src/main/java/boombimapi/domain/congestion/dto/response/MemberCongestionItemResponse.java
@@ -1,5 +1,24 @@
 package boombimapi.domain.congestion.dto.response;
 
-public record MemberCongestionItemResponse() {
+import boombimapi.domain.congestion.entity.MemberCongestion;
+import java.time.LocalDateTime;
+
+public record MemberCongestionItemResponse(
+    Long memberCongestionId,
+    String congestionLevelName,
+    String congestionLevelMessage,
+    LocalDateTime createdAt
+) {
+
+    public static MemberCongestionItemResponse of(
+        MemberCongestion memberCongestion
+    ) {
+        return new MemberCongestionItemResponse(
+            memberCongestion.getId(),
+            memberCongestion.getCongestionLevel().getName(),
+            memberCongestion.getCongestionMessage(),
+            memberCongestion.getCreatedAt()
+        );
+    }
 
 }
