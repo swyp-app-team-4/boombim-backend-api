5d630bced2c2e8d039df45772935f20077bbf8eb
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-02 22:29:42 +0900

    [Feat] Configure WebClient for Clova congestion message API in `WebConfig.java`

--- src/main/java/boombimapi/global/config/WebConfig.java
+++ src/main/java/boombimapi/global/config/WebConfig.java
@@ -2,15 +2,23 @@ package boombimapi.global.config;
 
 import boombimapi.domain.oauth2.domain.entity.SocialProvider;
 import boombimapi.global.config.converter.StringToSocialProviderConverter;
+import boombimapi.global.properties.ClovaCongestionMessageProperties;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(ClovaCongestionMessageProperties.class)
 public class WebConfig implements WebMvcConfigurer {
 
+    private final ClovaCongestionMessageProperties congestionMessageProperties;
     private final StringToSocialProviderConverter stringToSocialProviderConverter;
 
     @Override
@@ -19,4 +27,19 @@ public class WebConfig implements WebMvcConfigurer {
         // SocialProvider 대소문자 무시 변환
         registry.addConverter(String.class, SocialProvider.class, stringToSocialProviderConverter);
     }
+
+    @Bean(name = "clovaCongestionMessageClient")
+    public WebClient clovaCongestionMessageClient(
+        WebClient.Builder builder
+    ) {
+        return builder
+            .baseUrl(congestionMessageProperties.baseUrl())
+            .defaultHeaders(header -> {
+                header.setBearerAuth(congestionMessageProperties.apiKey());
+                header.setContentType(MediaType.APPLICATION_JSON);
+                header.setAccept(List.of(MediaType.APPLICATION_JSON));
+            })
+            .build();
+    }
+
 }
