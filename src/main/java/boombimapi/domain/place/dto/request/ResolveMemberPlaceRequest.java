6c258d7a082bf0ebf521f220a6837e5628cbe6ca
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-03 01:37:05 +0900

    [Feat] Add `address` field in `ResolveMemberPlaceRequest.java`

--- src/main/java/boombimapi/domain/place/dto/request/ResolveMemberPlaceRequest.java
+++ src/main/java/boombimapi/domain/place/dto/request/ResolveMemberPlaceRequest.java
@@ -1,21 +1,29 @@
 package boombimapi.domain.place.dto.request;
 
 public record ResolveMemberPlaceRequest(
-        String uuid,
-        String name,
-        Double latitude,
-        Double longitude,
-
-        String imageUrl
+    String uuid,
+    String name,
+    String address,
+    Double latitude,
+    Double longitude,
+    String imageUrl
 ) {
 
     public static ResolveMemberPlaceRequest of(
-            String uuid,
-            String name,
-            Double latitude,
-            Double longitude,
-            String imageUrl
-            ) {
-        return new ResolveMemberPlaceRequest(uuid, name, latitude, longitude, imageUrl);
+        String uuid,
+        String name,
+        String address,
+        Double latitude,
+        Double longitude,
+        String imageUrl
+    ) {
+        return new ResolveMemberPlaceRequest(
+            uuid,
+            name,
+            address,
+            latitude,
+            longitude,
+            imageUrl
+        );
     }
 }
