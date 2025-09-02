56b37f763e14cf7613984347f97a99d06524d506
chltmdgh522 <chltmdgh517@naver.com>
2025-09-02 23:19:26 +0900

    [Feat] Implement search service interface

--- src/main/java/boombimapi/domain/search/application/SearchService.java
+++ src/main/java/boombimapi/domain/search/application/SearchService.java
@@ -1,4 +1,20 @@
 package boombimapi.domain.search.application;
 
+import boombimapi.domain.search.presentation.dto.res.SearchHistoryRes;
+import boombimapi.domain.search.presentation.dto.res.SearchRelatedRes;
+import boombimapi.domain.search.presentation.dto.res.SearchRes;
+
+import java.util.List;
+
 public interface SearchService {
+
+    // 최근 검색 내역 10개 조회
+    List<SearchHistoryRes> getSearchHistory(String userId);
+
+    // 연관 검색어 조회
+    List<SearchRelatedRes> getSearchRelated(String posName);
+
+
+    // 검색 결과 조회 및 검색 내역 저장
+    List<SearchRes> getSearch(String posName);
 }
--- src/main/java/boombimapi/domain/search/application/impl/SearchServiceImpl.java
+++ src/main/java/boombimapi/domain/search/application/impl/SearchServiceImpl.java
@@ -1,4 +1,37 @@
 package boombimapi.domain.search.application.impl;
 
-public class SearchServiceImpl {
+import boombimapi.domain.search.application.SearchService;
+import boombimapi.domain.search.domain.repository.SearchRepository;
+import boombimapi.domain.search.presentation.dto.res.SearchHistoryRes;
+import boombimapi.domain.search.presentation.dto.res.SearchRelatedRes;
+import boombimapi.domain.search.presentation.dto.res.SearchRes;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class SearchServiceImpl implements SearchService {
+
+    private final SearchRepository searchRepository;
+
+    @Override
+    public List<SearchHistoryRes> getSearchHistory(String userId) {
+        return null;
+    }
+
+    @Override
+    public List<SearchRelatedRes> getSearchRelated(String posName) {
+        return null;
+    }
+
+    @Override
+    public List<SearchRes> getSearch(String posName) {
+        return null;
+    }
 }
