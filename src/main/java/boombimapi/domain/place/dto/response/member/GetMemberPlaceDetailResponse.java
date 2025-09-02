3d1d19b7e15eae78a567b442996eb59de7b8b38c
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-03 01:27:48 +0900

    [Feat] Implement response record for member place detail in `GetMemberPlaceDetailResponse.java`

--- src/main/java/boombimapi/domain/place/dto/response/member/GetMemberPlaceDetailResponse.java
+++ src/main/java/boombimapi/domain/place/dto/response/member/GetMemberPlaceDetailResponse.java
@@ -1,5 +1,30 @@
 package boombimapi.domain.place.dto.response.member;
 
-public record GetMemberPlaceDetailResponse() {
+import boombimapi.domain.congestion.dto.response.MemberCongestionItemResponse;
+import java.util.List;
+
+public record GetMemberPlaceDetailResponse(
+    MemberPlaceSummaryResponse memberPlaceSummary,
+    List<MemberCongestionItemResponse> memberCongestionItems,
+    boolean hasNext,
+    Long nextCursor,
+    int size
+) {
+
+    public static GetMemberPlaceDetailResponse of(
+        MemberPlaceSummaryResponse memberPlaceSummary,
+        List<MemberCongestionItemResponse> memberCongestionItems,
+        boolean hasNext,
+        Long nextCursor,
+        int size
+    ) {
+        return new GetMemberPlaceDetailResponse(
+            memberPlaceSummary,
+            memberCongestionItems,
+            hasNext,
+            nextCursor,
+            size
+        );
+    }
 
 }
