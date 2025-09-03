343da9d727a250bef92a1d329172a58e8eaa7d32
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-04 03:23:29 +0900

    [Feat] Implement `OfficialPlaceFavoriteResponse` record with factory method in `OfficialPlaceFavoriteResponse.java`

--- src/main/java/boombimapi/domain/favorite/dto/response/OfficialPlaceFavoriteResponse.java
+++ src/main/java/boombimapi/domain/favorite/dto/response/OfficialPlaceFavoriteResponse.java
@@ -1,5 +1,37 @@
 package boombimapi.domain.favorite.dto.response;
 
-public record OfficialPlaceFavoriteResponse() {
+import boombimapi.domain.place.entity.OfficialPlace;
+import boombimapi.domain.place.entity.PlaceType;
+import java.time.LocalDateTime;
+
+public record OfficialPlaceFavoriteResponse(
+    Long favoriteId,
+    Long placeId,
+    PlaceType placeType,
+    String name,
+    String imageUrl,
+    String congestionLevelName,
+    LocalDateTime observedAt,
+    boolean updatedToday
+) implements FavoriteResponse {
+
+    public static OfficialPlaceFavoriteResponse of(
+        Long favoriteId,
+        OfficialPlace officialPlace,
+        String congestionLevelName,
+        LocalDateTime observedAt,
+        boolean updatedToday
+    ) {
+        return new OfficialPlaceFavoriteResponse(
+            favoriteId,
+            officialPlace.getId(),
+            PlaceType.OFFICIAL_PLACE,
+            officialPlace.getName(),
+            officialPlace.getImageUrl(),
+            congestionLevelName,
+            observedAt,
+            updatedToday
+        );
+    }
 
 }
