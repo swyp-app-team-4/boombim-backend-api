6d38094d7f3cd321c824987c51f30fb8e4f618c6
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-04 00:27:45 +0900

    [Feat] Define validator interface with support type and validate method in `PlaceValidator.java`

--- src/main/java/boombimapi/domain/favorite/application/validator/PlaceValidator.java
+++ src/main/java/boombimapi/domain/favorite/application/validator/PlaceValidator.java
@@ -1,5 +1,13 @@
 package boombimapi.domain.favorite.application.validator;
 
+import boombimapi.domain.favorite.entity.FavoriteType;
+
 public interface PlaceValidator {
+
+    FavoriteType supports();
+
+    void validate(
+        Long placeId
+    );
 
 }
