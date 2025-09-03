66a20ae6d13c891b0a75ebdb96ae1e205a68c20c
chltmdgh522 <chltmdgh517@naver.com>
2025-09-03 17:32:07 +0900

    [Feat] Apply Swagger to search API

--- src/main/java/boombimapi/domain/search/presentation/controller/SearchController.java
+++ src/main/java/boombimapi/domain/search/presentation/controller/SearchController.java
@@ -7,6 +7,9 @@ import boombimapi.domain.search.presentation.dto.res.SearchRelatedRes;
 import boombimapi.domain.search.presentation.dto.res.SearchRes;
 import boombimapi.global.response.BaseOKResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -23,23 +26,32 @@ import static boombimapi.global.response.ResponseMessage.GET_ALARM_SUCCESS;
 @RequestMapping("/api/search")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Search", description = "검색 관련 API")
 public class SearchController {
 
     private final SearchService searchService;
-
-    @Operation(description = "검색 내역 조회 10개만")
+    @Operation(summary = "검색 내역 조회", description = "사용자가 입력한 검색 내역 조회합니다. ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "검색 내역 조회 성공"),
+    })
     @GetMapping("/history")
     public ResponseEntity<List<SearchHistoryRes>> getSearchHistory(@AuthenticationPrincipal String userId) {
         return ResponseEntity.ok(searchService.getSearchHistory(userId));
     }
 
-    @Operation(description = "연관검색어")
+    @Operation(summary = "연관 검색어", description = "연관 검색어가 나옵니다. 최대 20개까지 나옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "연관 검색어 조회 성공"),
+    })
     @GetMapping("/related")
     public ResponseEntity<List<SearchRelatedRes>> getSearchRelated(@RequestParam String posName) {
         return ResponseEntity.ok(searchService.getSearchRelated(posName));
     }
 
-    @Operation(description = "검색 결과 및 검색 내용 저장")
+    @Operation(summary = "검색 상세 조회", description = "검색 버튼을 누르면 실행되는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "검색 상세 조회 성공"),
+    })
     @GetMapping
     public ResponseEntity<List<SearchRes>> getSearch(
             @RequestParam String posName,
@@ -48,7 +60,11 @@ public class SearchController {
         return ResponseEntity.ok(searchService.getSearch(posName, userId));
     }
 
-    @Operation(description = "개인 삭제")
+    @Operation(summary = "검색 내역 개별 삭제", description = "검색 내역을 개별 삭제를 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "검색 삭제 성공"),
+            @ApiResponse(responseCode = "404", description = "검색 ID 존재하지가 않음"),
+    })
     @DeleteMapping
     public ResponseEntity<BaseOKResponse<Void>> deletePersonal(
             @AuthenticationPrincipal String userId,
@@ -60,7 +76,10 @@ public class SearchController {
                         DELETE_SEARCH_SUCCESS));
     }
 
-    @Operation(description = "전체 삭제")
+    @Operation(summary = "검색 내역 전체 삭제", description = "검색 내역을 전체를 삭제를 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "검색 전체 삭제 성공"),
+    })
     @DeleteMapping("/all")
     public ResponseEntity<BaseOKResponse<Void>> deleteAll(@AuthenticationPrincipal String userId) {
 
--- src/main/java/boombimapi/domain/search/presentation/dto/req/DeletePersonalReq.java
+++ src/main/java/boombimapi/domain/search/presentation/dto/req/DeletePersonalReq.java
@@ -1,6 +1,11 @@
 package boombimapi.domain.search.presentation.dto.req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "검색 개별 삭제 DTO")
 public record DeletePersonalReq(
+
+        @Schema(description = "검색 ID")
         Long searchId
 ) {
 }
