04244b6b12a4610312c0daa5423cb390be4f5821
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-04 03:20:40 +0900

    [Refactor] Seal `FavoriteResponse` interface and define common getters in `FavoriteResponse.java`

--- src/main/java/boombimapi/domain/favorite/dto/response/FavoriteResponse.java
+++ src/main/java/boombimapi/domain/favorite/dto/response/FavoriteResponse.java
@@ -1,5 +1,23 @@
 package boombimapi.domain.favorite.dto.response;
 
-public sealed interface FavoriteResponse {
+import boombimapi.domain.place.entity.PlaceType;
+import java.time.LocalDateTime;
+
+public sealed interface FavoriteResponse
+    permits MemberPlaceFavoriteResponse, OfficialPlaceFavoriteResponse {
+
+    Long favoriteId();
+
+    Long placeId();
+
+    PlaceType placeType();
+
+    String name();
+
+    String imageUrl();
+
+    String congestionLevelName();
+
+    LocalDateTime observedAt();
 
 }
