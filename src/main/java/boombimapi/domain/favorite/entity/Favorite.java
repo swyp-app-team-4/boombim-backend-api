c868b5e247202f4462661c0c3a7d6cfda512b76a
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-04 02:04:54 +0900

    [Refactor] Replace `memberPlace` relation with generic `placeId` and `placeType` in `Favorite.java`

--- src/main/java/boombimapi/domain/favorite/entity/Favorite.java
+++ src/main/java/boombimapi/domain/favorite/entity/Favorite.java
@@ -1,9 +1,12 @@
 package boombimapi.domain.favorite.entity;
 
 import boombimapi.domain.member.domain.entity.Member;
-import boombimapi.domain.place.entity.MemberPlace;
+import boombimapi.domain.place.entity.PlaceType;
 import boombimapi.global.base.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,26 +33,33 @@ public class Favorite extends BaseEntity {
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "member_place_id", nullable = false)
-    private MemberPlace memberPlace;
+    @Column(name = "place_id", nullable = false)
+    private Long placeId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "place_type", nullable = false, length = 20)
+    private PlaceType placeType;
 
     @Builder
     private Favorite(
         Member member,
-        MemberPlace memberPlace
+        Long placeId,
+        PlaceType placeType
     ) {
         this.member = member;
-        this.memberPlace = memberPlace;
+        this.placeId = placeId;
+        this.placeType = placeType;
     }
 
     public static Favorite of(
         Member member,
-        MemberPlace memberPlace
+        Long placeId,
+        PlaceType placeType
     ) {
         return Favorite.builder()
             .member(member)
-            .memberPlace(memberPlace)
+            .placeId(placeId)
+            .placeType(placeType)
             .build();
     }
 
