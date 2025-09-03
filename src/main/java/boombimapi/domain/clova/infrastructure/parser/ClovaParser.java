306a4c859bdfb43f71ac633c5eb70fbc2de68ece
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-03 20:22:15 +0900

    [Feat] Define `toText` method for converting response in `ClovaParser.java`

--- src/main/java/boombimapi/domain/clova/infrastructure/parser/ClovaParser.java
+++ src/main/java/boombimapi/domain/clova/infrastructure/parser/ClovaParser.java
@@ -1,5 +1,10 @@
 package boombimapi.domain.clova.infrastructure.parser;
 
+import boombimapi.domain.clova.dto.response.ClovaResponse;
+
 public interface ClovaParser {
 
+    String toText(
+        ClovaResponse clovaResponse
+    );
 }
