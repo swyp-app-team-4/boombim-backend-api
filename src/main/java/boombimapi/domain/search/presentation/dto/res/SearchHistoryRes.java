eb6bdc54fd029398ee66367f9491b6b71858aac9
chltmdgh522 <chltmdgh517@naver.com>
2025-09-03 17:39:36 +0900

    [Feat] Apply Swagger to search DTO

--- src/main/java/boombimapi/domain/search/presentation/dto/res/SearchHistoryRes.java
+++ src/main/java/boombimapi/domain/search/presentation/dto/res/SearchHistoryRes.java
@@ -1,8 +1,12 @@
 package boombimapi.domain.search.presentation.dto.res;
 
-public record SearchHistoryRes(
-        Long searchId,
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "검색 내역 조회")
+public record SearchHistoryRes(
+        @Schema(description = "검색 ID")
+        Long searchId,
+        @Schema(description = "검색 장소 이름")
         String posName
 ) {
     public static SearchHistoryRes of(Long searchId, String posName){
