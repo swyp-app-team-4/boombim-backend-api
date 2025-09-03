77cd7eb0a8acd46b3e8e4b4397b4088fbe10f0fc
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-03 10:18:06 +0900

    [Feat] Add fields `officialPlaceId`, `officialPlaceName`, `imageUrl` and factory method in `ViewportResponse.java`

--- src/main/java/boombimapi/domain/place/dto/response/ViewportResponse.java
+++ src/main/java/boombimapi/domain/place/dto/response/ViewportResponse.java
@@ -3,12 +3,33 @@ package boombimapi.domain.place.dto.response;
 import boombimapi.global.dto.Coordinate;
 
 public record ViewportResponse(
-    Long id,
-    String name,
+    Long officialPlaceId,
+    String officialPlaceName,
+    String imageUrl,
     Coordinate coordinate,
     double distance,
     String congestionLevelName,
     String congestionMessage
 ) {
+
+    public static ViewportResponse of(
+        Long officialPlaceId,
+        String officialPlaceName,
+        String imageUrl,
+        Coordinate coordinate,
+        double distance,
+        String congestionLevelName,
+        String congestionMessage
+    ) {
+        return new ViewportResponse(
+            officialPlaceId,
+            officialPlaceName,
+            imageUrl,
+            coordinate,
+            distance,
+            congestionLevelName,
+            congestionMessage
+        );
+    }
 
 }
