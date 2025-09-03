bf3dd6cd259e6f74c1bbafc46a90a68a9c993c7c
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-04 00:31:49 +0900

    [Feat] Implement member place validator with repository check in `MemberPlaceValidator.java`

--- src/main/java/boombimapi/domain/favorite/application/validator/MemberPlaceValidator.java
+++ src/main/java/boombimapi/domain/favorite/application/validator/MemberPlaceValidator.java
@@ -1,5 +1,26 @@
 package boombimapi.domain.favorite.application.validator;
 
-public class MemberPlaceValidator {
+import boombimapi.domain.favorite.entity.FavoriteType;
+import boombimapi.domain.place.repository.MemberPlaceRepository;
+import boombimapi.global.infra.exception.error.BoombimException;
+import boombimapi.global.infra.exception.error.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
+public class MemberPlaceValidator implements PlaceValidator {
+
+    private final MemberPlaceRepository memberPlaceRepository;
+
+    @Override
+    public FavoriteType supports() {
+        return FavoriteType.MEMBER_PLACE;
+    }
+
+    @Override
+    public void validate(Long placeId) {
+        memberPlaceRepository.findById(placeId)
+            .orElseThrow(() -> new BoombimException(ErrorCode.MEMBER_PLACE_NOT_FOUND));
+    }
 }
