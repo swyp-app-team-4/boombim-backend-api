ec40deb6dabd0216c3fc54c13b044b19001d22f7
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-03 20:24:58 +0900

    [Feat] Implement default Clova parser to extract content from response in `DefaultClovaParser.java`

--- src/main/java/boombimapi/domain/clova/infrastructure/parser/DefaultClovaParser.java
+++ src/main/java/boombimapi/domain/clova/infrastructure/parser/DefaultClovaParser.java
@@ -1,5 +1,16 @@
 package boombimapi.domain.clova.infrastructure.parser;
 
-public class DefaultClovaParser {
+import boombimapi.domain.clova.dto.response.ClovaResponse;
+import org.springframework.stereotype.Component;
 
+@Component
+public class DefaultClovaParser implements ClovaParser {
+
+    @Override
+    public String toText(
+        ClovaResponse clovaResponse
+    ) {
+
+        return clovaResponse.result().message().content();
+    }
 }
