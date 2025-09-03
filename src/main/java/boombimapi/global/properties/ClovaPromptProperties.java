d61ce4f3e02a2a8ece982dbc0f81c64bab7073d5
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-03 20:22:37 +0900

    [Feat] Add Clova prompt configuration properties with `autoCompleteCongestionMessage` field in `ClovaPromptProperties.java`

--- src/main/java/boombimapi/global/properties/ClovaPromptProperties.java
+++ src/main/java/boombimapi/global/properties/ClovaPromptProperties.java
@@ -1,5 +1,10 @@
 package boombimapi.global.properties;
 
-public record ClovaPromptProperties() {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "clova.prompts")
+public record ClovaPromptProperties(
+    String autoCompleteCongestionMessage
+) {
 
 }
