dad86ca4d256afbb7b210366631d4a3bb31561d4
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-04 21:44:18 +0900

    [Feat] Add projection fields for NearbyOfficialPlace query in `NearbyOfficialPlaceProjection.java`

--- src/main/java/boombimapi/domain/place/repository/projection/NearbyOfficialPlaceProjection.java
+++ src/main/java/boombimapi/domain/place/repository/projection/NearbyOfficialPlaceProjection.java
@@ -2,4 +2,14 @@ package boombimapi.domain.place.repository.projection;
 
 public interface NearbyOfficialPlaceProjection {
 
+    Long getId();
+
+    String getName();
+
+    String getImageUrl();
+
+    Double getDistanceMeters();
+
+    String getCongestionLevelName();
+
 }
