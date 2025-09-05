b313441ba57cee2ff724413abc31609b73b4a10c
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-05 17:27:05 +0900

    [Refactor] Add `legalDong` and `observedAt` fields to NearbyOfficialPlaceProjection in `NearbyOfficialPlaceProjection.java`

--- src/main/java/boombimapi/domain/place/repository/projection/NearbyOfficialPlaceProjection.java
+++ src/main/java/boombimapi/domain/place/repository/projection/NearbyOfficialPlaceProjection.java
@@ -1,14 +1,20 @@
 package boombimapi.domain.place.repository.projection;
 
+import java.time.LocalDateTime;
+
 public interface NearbyOfficialPlaceProjection {
 
     Long getId();
 
     String getName();
 
+    String getLegalDong();
+
     String getImageUrl();
 
     Double getDistanceMeters();
+
+    LocalDateTime getObservedAt();
 
     String getCongestionLevelName();
 
