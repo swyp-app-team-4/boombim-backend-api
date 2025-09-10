38e2fc8e6b4549cca176fa8e91678b765a85d1e5
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-10 22:21:23 +0900

    [Feat] Add `@ConfigurationProperties` support to `AiAttemptTokenBucketProperties.java`

--- src/main/java/boombimapi/global/properties/AiAttemptTokenBucketProperties.java
+++ src/main/java/boombimapi/global/properties/AiAttemptTokenBucketProperties.java
@@ -1,5 +1,8 @@
 package boombimapi.global.properties;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "rate-limit.ai.token-bucket")
 public record AiAttemptTokenBucketProperties(
     int capacity,
     double refillPerSecond,
