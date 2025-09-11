dff2975523a0c169c01a686303d904000fe0bb72
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-11 11:53:01 +0900

    [Feat] Add optional `retryAt` field with builder in `ErrorResponse.java`

--- src/main/java/boombimapi/global/infra/exception/error/response/ErrorResponse.java
+++ src/main/java/boombimapi/global/infra/exception/error/response/ErrorResponse.java
@@ -2,42 +2,69 @@ package boombimapi.global.infra.exception.error.response;
 
 import boombimapi.global.infra.exception.error.BoombimException;
 import boombimapi.global.infra.exception.error.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.time.LocalDateTime;
+import lombok.Builder;
 
-
+@Builder
+@JsonInclude(Include.NON_NULL)
 public record ErrorResponse(
-        int status,
-        int code,
-        String message,
-        LocalDateTime time
+    int status,
+    int code,
+    String message,
+    LocalDateTime time,
+
+    // optional
+    LocalDateTime retryAt
 ) {
-    public static ErrorResponse of(BoombimException exception) {
-        return new ErrorResponse(
-                exception.getHttpStatusCode(),
-                exception.getErrorCode().getCode(),
-                exception.getMessage(),
 
-                LocalDateTime.now()
-        );
+    public static ErrorResponse of(
+        BoombimException e
+    ) {
+        return ErrorResponse.builder()
+            .status(e.getHttpStatusCode())
+            .code(e.getErrorCode().getCode())
+            .message(e.getMessage())
+            .time(LocalDateTime.now())
+            .build();
     }
 
-    public static ErrorResponse of(ErrorCode errorCode) {
-        return new ErrorResponse(
-                errorCode.getHttpCode(),
-                errorCode.getCode(),
-                errorCode.getMessage(),
-
-                LocalDateTime.now()
-        );
+    public static ErrorResponse of(
+        ErrorCode errorCode
+    ) {
+        return ErrorResponse.builder()
+            .status(errorCode.getHttpCode())
+            .code(errorCode.getCode())
+            .message(errorCode.getMessage())
+            .time(LocalDateTime.now())
+            .build();
     }
 
-    public static ErrorResponse of(ErrorCode errorCode, String customMessage) {
-        return new ErrorResponse(
-                errorCode.getHttpCode(),
-                errorCode.getCode(),
-                customMessage,
-
-                LocalDateTime.now()
-        );
+    public static ErrorResponse of(
+        ErrorCode errorCode,
+        String customMessage
+    ) {
+        return ErrorResponse.builder()
+            .status(errorCode.getHttpCode())
+            .code(errorCode.getCode())
+            .message(customMessage)
+            .time(LocalDateTime.now())
+            .build();
     }
+
+    public static ErrorResponse of(
+        ErrorCode errorCode,
+        LocalDateTime now,
+        LocalDateTime retryAt
+    ) {
+        return ErrorResponse.builder()
+            .status(errorCode.getHttpCode())
+            .code(errorCode.getCode())
+            .message(errorCode.getMessage())
+            .time(now)
+            .retryAt(retryAt)
+            .build();
+    }
+
 }
