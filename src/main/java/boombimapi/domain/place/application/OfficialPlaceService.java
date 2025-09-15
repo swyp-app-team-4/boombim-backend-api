85c3f0566db8d027047372bb3121edabc8165fe1
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-15 11:29:56 +0900

    [Chore] Remove debug logging for viewport request details in `OfficialPlaceService.java`

--- src/main/java/boombimapi/domain/place/application/OfficialPlaceService.java
+++ src/main/java/boombimapi/domain/place/application/OfficialPlaceService.java
@@ -48,9 +48,9 @@ public class OfficialPlaceService {
         ViewportRequest request
     ) {
 
-        log.info("[OfficialPlaceService] getOfficialPlacesInViewport() topLeft: {}", request.topLeft());
-        log.info("[OfficialPlaceService] getOfficialPlacesInViewport() bottomRight: {}", request.bottomRight());
-        log.info("[OfficialPlaceService] getOfficialPlacesInViewport() zoomLevel: {}", request.zoomLevel());
+//        log.info("[OfficialPlaceService] getOfficialPlacesInViewport() topLeft: {}", request.topLeft());
+//        log.info("[OfficialPlaceService] getOfficialPlacesInViewport() bottomRight: {}", request.bottomRight());
+//        log.info("[OfficialPlaceService] getOfficialPlacesInViewport() zoomLevel: {}", request.zoomLevel());
 
         // TODO: 직선 거리 계산 부분 리팩터링 필요
         // 1. 뷰포트 범위 계산
