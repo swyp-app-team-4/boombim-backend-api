a1e53189645433a3cc45c3ad0530a3874dcfea08
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-17 23:15:00 +0900

    [Feat] Add viewport query method in `OfficialPlaceViewportDao.java`

--- src/main/java/boombimapi/domain/place/query/dao/OfficialPlaceViewportDao.java
+++ src/main/java/boombimapi/domain/place/query/dao/OfficialPlaceViewportDao.java
@@ -1,5 +1,16 @@
 package boombimapi.domain.place.query.dao;
 
+import boombimapi.domain.place.query.dao.row.OfficialPlaceViewportRow;
+import java.util.List;
+
 public interface OfficialPlaceViewportDao {
+
+    List<OfficialPlaceViewportRow> findInViewport(
+        String memberId,
+        double minLatitude,
+        double maxLatitude,
+        double minLongitude,
+        double maxLongitude
+    );
 
 }
