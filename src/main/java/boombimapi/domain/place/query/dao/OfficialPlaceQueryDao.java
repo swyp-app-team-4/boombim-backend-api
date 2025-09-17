7b290fe35c93f8bacf16f5b824f5cbeb4393df60
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-17 23:19:47 +0900

    [Refactor] Rename to `OfficialPlaceQueryDao.java`

--- src/main/java/boombimapi/domain/place/query/dao/OfficialPlaceQueryDao.java
+++ src/main/java/boombimapi/domain/place/query/dao/OfficialPlaceQueryDao.java
@@ -3,7 +3,7 @@ package boombimapi.domain.place.query.dao;
 import boombimapi.domain.place.query.dao.row.OfficialPlaceViewportRow;
 import java.util.List;
 
-public interface OfficialPlaceViewportDao {
+public interface OfficialPlaceQueryDao {
 
     List<OfficialPlaceViewportRow> findInViewport(
         String memberId,
