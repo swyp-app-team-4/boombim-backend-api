3ff52fdfd9222704e2ab4f5a0c25107fadbe9d16
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-18 01:34:33 +0900

    [Refactor] Change findViewport() to use OfficialPlaceViewportParam in `OfficialPlaceQueryDao.java`

--- src/main/java/boombimapi/domain/place/query/dao/OfficialPlaceQueryDao.java
+++ src/main/java/boombimapi/domain/place/query/dao/OfficialPlaceQueryDao.java
@@ -1,16 +1,13 @@
 package boombimapi.domain.place.query.dao;
 
+import boombimapi.domain.place.query.dao.param.OfficialPlaceViewportParam;
 import boombimapi.domain.place.query.dao.row.OfficialPlaceViewportRow;
 import java.util.List;
 
 public interface OfficialPlaceQueryDao {
 
     List<OfficialPlaceViewportRow> findInViewport(
-        String memberId,
-        double minLatitude,
-        double maxLatitude,
-        double minLongitude,
-        double maxLongitude
+        OfficialPlaceViewportParam officialPlaceViewportParam
     );
 
 }
