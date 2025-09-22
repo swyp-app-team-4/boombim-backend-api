395c7a2c96665f028940cde00434bc6a72fc5e55
chltmdgh522 <chltmdgh517@naver.com>
2025-09-22 10:44:19 +0900

    [Fix] Add 300m restriction

--- src/main/java/boombimapi/domain/vote/application/service/impl/VoteServiceImpl.java
+++ src/main/java/boombimapi/domain/vote/application/service/impl/VoteServiceImpl.java
@@ -231,12 +231,12 @@ public class VoteServiceImpl implements VoteService {
         /**
          * QA 떄문에 잠시 닫겠음
          */
-        // List<Vote> votes = calculate300(latitude, longitude);
+        List<Vote> votes = calculate300(latitude, longitude);
 
         /**
          * QA 끝나면 주석 처리 할 예정
          */
-        List<Vote> votes = voteRepository.findAll();
+        // List<Vote> votes = voteRepository.findAll();
 
         for (Vote vote : votes) {
             if (!vote.isVoteActivate() || vote.getVoteStatus().equals(VoteStatus.END))
