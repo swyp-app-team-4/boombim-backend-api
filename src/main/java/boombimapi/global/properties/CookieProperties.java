76c4fe542418523d3d74ea557dd6890707bac64b
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-10 17:38:58 +0900

    [Feat] Add configuration records in `CookieProperties.java`

--- src/main/java/boombimapi/global/properties/CookieProperties.java
+++ src/main/java/boombimapi/global/properties/CookieProperties.java
@@ -1,5 +1,16 @@
 package boombimapi.global.properties;
 
-public record CookieProperties() {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "auth.cookie")
+public record CookieProperties(
+    boolean secure,
+    String atName,
+    String rtName,
+    Long atMaxAgeMillis,
+    Long rtMaxAgeMillis,
+    String sameSite,
+    String frontRedirect
+) {
 
 }
