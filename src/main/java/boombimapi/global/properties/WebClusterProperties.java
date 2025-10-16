d20899ac4c623a4cf5f621f83ac121a8a73dd108
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-16 11:38:34 +0900

    [Refactor] Relocate to `global.properties`

--- src/main/java/boombimapi/global/properties/WebClusterProperties.java
+++ src/main/java/boombimapi/global/properties/WebClusterProperties.java
@@ -1,9 +1,9 @@
-package boombimapi.domain.place.cluster;
+package boombimapi.global.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "cluster.web")
-public record WebClusterPolicy(
+public record WebClusterProperties(
     int zMappingBase,
     int baseCellPixel,
     double tileSize
