dd17999bf8e274e8372a2d27efc5e7e41c4f6e1d
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-16 12:40:39 +0900

    [Refactor] Remove unnecessary parameters in `Clusterer.java`

--- src/main/java/boombimapi/domain/place/cluster/Clusterer.java
+++ src/main/java/boombimapi/domain/place/cluster/Clusterer.java
@@ -1,17 +1,13 @@
 package boombimapi.domain.place.cluster;
 
-import boombimapi.global.geo.core.ClusterInput;
-import boombimapi.global.geo.core.ClusterResult;
+import boombimapi.domain.place.cluster.vo.ClusterInput;
+import boombimapi.domain.place.cluster.vo.ClusterResult;
 import java.util.List;
 
 public interface Clusterer {
 
     List<ClusterResult> cluster(
         List<ClusterInput> clusterInputs,
-        double minLatitude,
-        double maxLatitude,
-        double minLongitude,
-        double maxLongitude,
         int zoomLevel
     );
 
