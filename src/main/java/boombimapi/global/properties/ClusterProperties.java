fe7ce1130812c189cc82ddb1664e949e922aaf3d
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-17 16:55:00 +0900

    [Feat] Define sealed interface with core cluster configuration accessors in `ClusterProperties.java`

--- src/main/java/boombimapi/global/properties/ClusterProperties.java
+++ src/main/java/boombimapi/global/properties/ClusterProperties.java
@@ -1,5 +1,15 @@
 package boombimapi.global.properties;
 
-public interface ClusterProperties {
+public sealed interface ClusterProperties
+    permits WebClusterProperties, AppClusterProperties {
 
+    int refZ();
+
+    int maxZoomAtRefZ();
+
+    int baseCellPixel();
+
+    double tileSize();
+
+    int minClusterSize();
 }
