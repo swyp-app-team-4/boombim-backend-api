3f3b0c67412c5d3a2c937a173e5c93a542cc609e
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-17 17:00:52 +0900

    [Refactor] Unify route under /api and add documentation in `OfficialPlaceQueryController.java`

--- src/main/java/boombimapi/domain/place/query/api/controller/OfficialPlaceQueryController.java
+++ src/main/java/boombimapi/domain/place/query/api/controller/OfficialPlaceQueryController.java
@@ -6,6 +6,9 @@ import boombimapi.domain.place.query.api.dto.request.ViewportRequest;
 import boombimapi.domain.place.query.api.dto.response.ViewportResponse;
 import boombimapi.domain.place.query.service.OfficialPlaceQueryService;
 import boombimapi.global.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,12 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/query/official-place")
+@RequestMapping("/api")
 public class OfficialPlaceQueryController {
 
     private final OfficialPlaceQueryService officialPlaceQueryService;
 
-    @PostMapping
+    // TODO: 공식 장소에도 클러스터링이 도입된다면, app용 엔드포인트 하나 추가해야함
+
+    @Operation(summary = "뷰포트 내 공식 장소 조회", description = "뷰포트 내 공식 장소들의 정보를 리스트로 반환합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "뷰포트 내 공식 장소 조회 성공")
+    })
+    @PostMapping("/web/official-place")
     public ResponseEntity<BaseResponse<List<ViewportResponse>>> getOfficialPlacesInViewport(
         @AuthenticationPrincipal String memberId,
         @RequestBody ViewportRequest request
