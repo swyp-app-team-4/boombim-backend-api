760a8e670f26fb023debaa3c042680ee86fbd6aa
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-18 15:46:45 +0900

    [Feat] Implement dynamic name and maxAge resolution in `AuthCookieType.java`

--- src/main/java/boombimapi/domain/oauth2/cookie/type/AuthCookieType.java
+++ src/main/java/boombimapi/domain/oauth2/cookie/type/AuthCookieType.java
@@ -1,4 +1,33 @@
 package boombimapi.domain.oauth2.cookie.type;
 
+import boombimapi.global.properties.CookieProperties;
+import java.util.function.Function;
+import java.util.function.ToLongFunction;
+
 public enum AuthCookieType {
+    ACCESS(CookieProperties::atName, CookieProperties::atMaxAgeMillis),
+    REFRESH(CookieProperties::rtName, CookieProperties::rtMaxAgeMillis);
+
+    private final Function<CookieProperties, String> nameFunction;
+    private final ToLongFunction<CookieProperties> maxAgeFunction;
+
+    AuthCookieType(
+        Function<CookieProperties, String> nameFunction,
+        ToLongFunction<CookieProperties> maxAgeFunction
+    ) {
+        this.nameFunction = nameFunction;
+        this.maxAgeFunction = maxAgeFunction;
+    }
+
+    public String nameFrom(
+        CookieProperties properties
+    ) {
+        return nameFunction.apply(properties);
+    }
+
+    public long maxAgeFrom(
+        CookieProperties properties
+    ) {
+        return maxAgeFunction.applyAsLong(properties);
+    }
 }
