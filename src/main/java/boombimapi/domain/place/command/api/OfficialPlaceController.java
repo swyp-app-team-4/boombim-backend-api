6f88d98f1d3f54a2c8ccdc62cbd7428f8aaf73f8
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-18 15:53:55 +0900

    [Refactor] Deprecate unused viewport endpoint and simplify `OfficialPlaceController.java`

--- src/main/java/boombimapi/domain/place/command/api/OfficialPlaceController.java
+++ src/main/java/boombimapi/domain/place/command/api/OfficialPlaceController.java
@@ -3,11 +3,9 @@ package boombimapi.domain.place.command.api;
 import static boombimapi.global.response.ResponseMessage.*;
 
 import boombimapi.domain.place.command.service.OfficialPlaceService;
-import boombimapi.domain.place.query.api.dto.request.ViewportRequest;
 import boombimapi.domain.place.query.api.dto.response.official.CongestedOfficialPlaceResponse;
 import boombimapi.domain.place.query.api.dto.response.official.NearbyNonCongestedOfficialPlaceResponse;
 import boombimapi.domain.place.query.api.dto.response.official.OfficialPlaceOverviewResponse;
-import boombimapi.domain.place.query.api.dto.response.ViewportResponse;
 import boombimapi.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -20,8 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,23 +31,23 @@ public class OfficialPlaceController {
     private final OfficialPlaceService officialPlaceService;
 
     // TODO: deprecated됨 -> 프론트에 전달 후 삭제 예정
-    @Operation(summary = "뷰포트 내 공식 장소 조회", description = "뷰포트 내 공식 장소들의 정보를 리스트로 반환합니다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "뷰포트 내 공식 장소 조회 성공")
-    })
-    @PostMapping
-    public ResponseEntity<BaseResponse<List<ViewportResponse>>> getOfficialPlacesInViewport(
-        @AuthenticationPrincipal String memberId,
-        @RequestBody ViewportRequest request
-    ) {
-        return ResponseEntity.ok(
-            BaseResponse.of(
-                HttpStatus.OK,
-                GET_OFFICIAL_PLACES_IN_VIEWPORT_SUCCESS,
-                officialPlaceService.getOfficialPlacesInViewport(memberId, request)
-            )
-        );
-    }
+//    @Operation(summary = "뷰포트 내 공식 장소 조회", description = "뷰포트 내 공식 장소들의 정보를 리스트로 반환합니다.")
+//    @ApiResponses(value = {
+//        @ApiResponse(responseCode = "200", description = "뷰포트 내 공식 장소 조회 성공")
+//    })
+//    @PostMapping
+//    public ResponseEntity<BaseResponse<List<ViewportResponse>>> getOfficialPlacesInViewport(
+//        @AuthenticationPrincipal String memberId,
+//        @RequestBody ViewportRequest request
+//    ) {
+//        return ResponseEntity.ok(
+//            BaseResponse.of(
+//                HttpStatus.OK,
+//                GET_OFFICIAL_PLACES_IN_VIEWPORT_SUCCESS,
+//                officialPlaceService.getOfficialPlacesInViewport(memberId, request)
+//            )
+//        );
+//    }
 
     @Operation(summary = "특정 공식 장소 정보 조회", description = "특정 공식 장소의 인구 통계 및 예상 혼잡도를 반환합니다.")
     @ApiResponses(value = {
