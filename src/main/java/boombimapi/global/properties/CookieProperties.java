737ddbe4675302da8b6e5fc547fc1af880219621
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-18 15:48:14 +0900

    [Refactor] Add path field and reorder properties for clarity in `CookieProperties.java`

--- src/main/java/boombimapi/global/properties/CookieProperties.java
+++ src/main/java/boombimapi/global/properties/CookieProperties.java
@@ -5,11 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "auth.cookie")
 public record CookieProperties(
     boolean secure,
+    String sameSite,
     String atName,
     String rtName,
     Long atMaxAgeMillis,
     Long rtMaxAgeMillis,
-    String sameSite,
+    String path,
     String frontRedirect
 ) {
 
