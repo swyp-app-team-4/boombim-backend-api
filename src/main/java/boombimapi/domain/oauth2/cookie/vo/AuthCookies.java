9752f6b2faca89dcb21547bb5b760ba17050cbba
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-18 15:44:02 +0900

    [Refactor] Extend to support login, reissue, and logout cookie states in `AuthCookies.java`

--- src/main/java/boombimapi/domain/oauth2/cookie/vo/AuthCookies.java
+++ src/main/java/boombimapi/domain/oauth2/cookie/vo/AuthCookies.java
@@ -3,17 +3,37 @@ package boombimapi.domain.oauth2.cookie.vo;
 import org.springframework.http.ResponseCookie;
 
 public record AuthCookies(
-    ResponseCookie accessToken,
-    ResponseCookie refreshToken
+    ResponseCookie accessTokenCookie,
+    ResponseCookie refreshTokenCookie
 ) {
 
-    public static AuthCookies of(
-        ResponseCookie accessToken,
-        ResponseCookie refreshToken
+    public static AuthCookies login(
+        ResponseCookie accessTokenCookie,
+        ResponseCookie refreshTokenCookie
     ) {
         return new AuthCookies(
-            accessToken,
-            refreshToken
+            accessTokenCookie,
+            refreshTokenCookie
+        );
+    }
+
+    public static AuthCookies reissue(
+        ResponseCookie accessTokenCookie,
+        ResponseCookie refreshTokenCookie
+    ) {
+        return new AuthCookies(
+            accessTokenCookie,
+            refreshTokenCookie
+        );
+    }
+
+    public static AuthCookies logout(
+        ResponseCookie logoutAccessTokenCookie,
+        ResponseCookie logoutRefreshTokenCookie
+    ) {
+        return new AuthCookies(
+            logoutAccessTokenCookie,
+            logoutRefreshTokenCookie
         );
     }
 
