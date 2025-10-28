f4617e37fe2766ed3028b07f6df2d86afa4ac557
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-10-28 17:33:29 +0900

    [Feat] Implement refresh token VO with static factory method in `AuthCookie.java`

--- src/main/java/boombimapi/domain/oauth2/cookie/vo/AuthCookie.java
+++ src/main/java/boombimapi/domain/oauth2/cookie/vo/AuthCookie.java
@@ -1,5 +1,17 @@
 package boombimapi.domain.oauth2.cookie.vo;
 
-public record AuthCookie() {
+import org.springframework.http.ResponseCookie;
+
+public record AuthCookie(
+    ResponseCookie refreshTokenCookie
+) {
+    public static AuthCookie createRefreshTokenCookie(
+        ResponseCookie refreshTokenCookie
+    ) {
+        return new AuthCookie(
+            refreshTokenCookie
+        );
+    }
+
 
 }
