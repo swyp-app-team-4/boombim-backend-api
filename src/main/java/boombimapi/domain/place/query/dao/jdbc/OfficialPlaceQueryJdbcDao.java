7f33d311bfcea44e778f4259f250e66ea01800ff
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-12-10 23:27:52 +0900

    [Refactor] Replace query in `OfficialPlaceQueryJdbcDao.java`

--- src/main/java/boombimapi/domain/place/query/dao/jdbc/OfficialPlaceQueryJdbcDao.java
+++ src/main/java/boombimapi/domain/place/query/dao/jdbc/OfficialPlaceQueryJdbcDao.java
@@ -17,6 +17,58 @@ public class OfficialPlaceQueryJdbcDao implements OfficialPlaceQueryDao {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
+//    @Override
+//    public List<OfficialPlaceViewportRow> findInViewport(
+//        ViewportParam param
+//    ) {
+//        String sql = """
+//            SELECT
+//                op.id,
+//                op.name,
+//                op.legal_dong,
+//                op.image_url,
+//                op.centroid_latitude,
+//                op.centroid_longitude,
+//                cl.name    AS congestion_level_name,
+//                cl.message AS congestion_message,
+//                oc.observed_at AS observed_at,
+//                CASE
+//                  WHEN :memberId IS NULL THEN FALSE
+//                  WHEN f.place_id IS NOT NULL THEN TRUE
+//                  ELSE FALSE
+//                END AS is_favorite
+//            FROM official_places op
+//            LEFT JOIN LATERAL (
+//               SELECT oc.*
+//               FROM official_congestions oc
+//               WHERE oc.official_place_id = op.id
+//               ORDER BY oc.observed_at DESC
+//               LIMIT 1
+//            ) oc ON TRUE
+//            LEFT JOIN congestion_levels cl ON cl.id = oc.congestion_level_id
+//            LEFT JOIN favorites f
+//              ON f.member_id = :memberId
+//             AND f.place_id  = op.id
+//             AND f.place_type = 'OFFICIAL_PLACE'
+//            WHERE op.centroid_latitude  BETWEEN :minLat AND :maxLat
+//              AND op.centroid_longitude BETWEEN :minLng AND :maxLng
+//            """;
+//
+//        Map<String, Object> params = Map.of(
+//            "memberId", param.memberId(),
+//            "minLat", param.minLatitude(),
+//            "maxLat", param.maxLatitude(),
+//            "minLng", param.minLongitude(),
+//            "maxLng", param.maxLongitude()
+//        );
+//
+//        return jdbcTemplate.query(
+//            sql,
+//            params,
+//            OFFICIAL_PLACE_VIEWPORT
+//        );
+//    }
+
     @Override
     public List<OfficialPlaceViewportRow> findInViewport(
         ViewportParam param
@@ -29,27 +81,35 @@ public class OfficialPlaceQueryJdbcDao implements OfficialPlaceQueryDao {
                 op.image_url,
                 op.centroid_latitude,
                 op.centroid_longitude,
-                cl.name    AS congestion_level_name,
-                cl.message AS congestion_message,
+                cl.name        AS congestion_level_name,
+                cl.message     AS congestion_message,
                 oc.observed_at AS observed_at,
-                CASE 
-                  WHEN :memberId IS NULL THEN FALSE
-                  WHEN f.place_id IS NOT NULL THEN TRUE
-                  ELSE FALSE
+                CASE
+                    WHEN :memberId IS NULL THEN FALSE
+                    WHEN f.place_id IS NOT NULL THEN TRUE
+                    ELSE FALSE
                 END AS is_favorite
             FROM official_places op
-            LEFT JOIN LATERAL (
-               SELECT oc.*
-               FROM official_congestions oc
-               WHERE oc.official_place_id = op.id
-               ORDER BY oc.observed_at DESC
-               LIMIT 1
-            ) oc ON TRUE
-            LEFT JOIN congestion_levels cl ON cl.id = oc.congestion_level_id
+            LEFT JOIN (
+                SELECT *
+                FROM (
+                    SELECT
+                        oc.*,
+                        ROW_NUMBER() OVER (
+                            PARTITION BY oc.official_place_id
+                            ORDER BY oc.observed_at DESC
+                        ) AS rn
+                    FROM official_congestions oc
+                ) t
+                WHERE t.rn = 1
+            ) oc
+                ON oc.official_place_id = op.id
+            LEFT JOIN congestion_levels cl
+                ON cl.id = oc.congestion_level_id
             LEFT JOIN favorites f
-              ON f.member_id = :memberId
-             AND f.place_id  = op.id
-             AND f.place_type = 'OFFICIAL_PLACE'
+                ON f.member_id = :memberId
+               AND f.place_id  = op.id
+               AND f.place_type = 'OFFICIAL_PLACE'
             WHERE op.centroid_latitude  BETWEEN :minLat AND :maxLat
               AND op.centroid_longitude BETWEEN :minLng AND :maxLng
             """;
